05adb413012a0ba6ccbdb9f3af7f4f0c3f50d26a
Bethly9996 <bethlyc@kabarak.ac.ke>
2023-01-26 14:30:08 +0300

    useRef hook

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 
-import React, { useState, useEffect } from 'react'
+import { render } from '@testing-library/react';
+import React, { useState, useEffect, useRef } from 'react'
 
 export default function App() {
-
-  const [resourceType, setResourceType] = useState('posts');
-
  
-   
-  useEffect(() => {
-   console.log('resource changed');
+  const [name, setName] = useState('');
+  const inputRef = useRef();
 
-   return () => {
-    console.log('return from resource change')
-   }
-  } , [resourceType]);
+const focus = () => {
+  inputRef.current.focus()
+}
 
 return (
   <>
-  <div>
-    <button onClick={() => setResourceType('posts')}>Posts</button>
-    <button onClick={() => setResourceType('users')}>Users</button>
-    <button onClick={() => setResourceType('comments')}>Comments</button>
-  </div>
-  <h1>{resourceType}</h1>
+  <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}/>
+  <div>My name is {name}</div>
+  <button onClick={focus}>Focus</button>
   
   </>
 )
