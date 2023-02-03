da1e820564de495405e2d96ba3b3e92cbd469f41
Bethly9996 <bethlyc@kabarak.ac.ke>
2023-02-03 11:13:54 +0300

    parse query params

--- express/routes/users.js
+++ express/routes/users.js
@@ -2,37 +2,42 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+    console.log(req.query.name)
   res.send("User List");
 });
 
 router.get("/new", (req, res) => {
-  res.render("users/new")
+  res.render("users/new");
 });
 
 router.post("/", (req, res) => {
-    console.log(req.body.firstName)
-  res.send("Hi");
+  const isValid = true;
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("Error");
+    res.render("users/new", { firstName: req.body.firstName });
+  }
 });
 
 router
   .route("/:id")
   .get((req, res) => {
-    console.log(req.user)
+    console.log(req.user);
     res.send(`Get user with ID ${req.params.id}`);
   })
   .put((req, res) => {
     res.send(`Update user with ID ${req.params.id}`);
   })
   .delete((req, res) => {
-    
     res.send(`Delete User With ID  ${req.params.id}`);
-  }); 
-  const users = [{name: "Bethly"}, {name: "Sally"}];
+  });
+const users = [{ name: "Bethly" }, { name: "Sally" }];
 
-  router.param("id", (req, res, next, id) => {
-    req.user = users[id];
-    next()
-  })
-  
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
+});
 
 module.exports = router;
